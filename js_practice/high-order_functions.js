2d4037c60694152128445be321f178867e94bcea
I.Srimanth Raj <sreemonth4@gmail.com>
2024-02-14 17:46:34 +0000

    updated using arrow functions

---
+++ js_practice/high-order_functions.js
@@ -0,0 +1,13 @@
+a=5
+console.log(a)
+
+//converting rupees to dollars
+
+const tans=[100,2000,5793,3000,6500]
+const dollar_value=80
+
+let convert=tans.map((amount)=>{
+    return amount/dollar_value
+})
+
+console.log(convert)
