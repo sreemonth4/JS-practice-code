42186202b9b41b49c51cf8b4103d7ec24de18631
I.Srimanth Raj <sreemonth4@gmail.com>
2024-02-14 17:49:29 +0000

    added .tofixed

--- js_practice/high-order_functions.js
+++ js_practice/high-order_functions.js
@@ -7,7 +7,7 @@ const tans=[100,2000,5793,3000,6500]
 const dollar_value=80
 
 let convert=tans.map((amount)=>{
-    return amount/dollar_value
+    return (amount/dollar_value).toFixed(0)
 })
 
 console.log(convert)
