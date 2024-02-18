3d3ddddefd894070bcd2f72f14cfa64e854155a1
I.Srimanth Raj <sreemonth4@gmail.com>
2024-02-18 23:06:23 +0530

    Add files via upload

---
+++ js_practice/basic.js
@@ -0,0 +1,7 @@
+
+
+ let a=5
+console.log(a);
+
+let b=  10  //  number  of seconds  to wait for before      
+ console.log(b);
---
+++ js_practice/filter.js
@@ -0,0 +1,12 @@
+let numbers=[100,300,-455,9,34,-33,52,-3,-2,83]
+
+let greaterThan=numbers.filter(function(n){
+    return n>0
+}
+)
+console.log(greaterThan)
+
+
+
+
+
